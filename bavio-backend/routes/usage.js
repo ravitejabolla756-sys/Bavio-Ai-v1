a3362de1b44b0c0e7ab4499eef3dbde3a35000e3
Ravi Teja Bolla <rb7663@srmist.edu.in>
2026-06-06 12:40:05 +0530

    Delete bavio-backend directory

--- bavio-backend/routes/usage.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const analyticsController = require('../controllers/analyticsController');
-const { requireAuth } = require('../middleware/auth');
-
-router.get('/:client_id', requireAuth, analyticsController.getUsage);
-
-module.exports = router;
